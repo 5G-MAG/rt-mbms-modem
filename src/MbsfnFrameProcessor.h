87e685ef0e1f7de286b30d94898ccaf4608e8b5b
Jaime Sánchez Roldán <jaisanro@iteam.upv.es>
2025-05-07 12:58:02 +0200

    Add missing option to allow non contiguous RRC SN across multiple MCH scheduling periods
    Missing from previous version made by Klaus.

--- src/MbsfnFrameProcessor.h
+++ src/MbsfnFrameProcessor.h
@@ -54,7 +54,10 @@ class MbsfnFrameProcessor {
       , _rest(rest)
       , mch_mac_msg(20, log_h)
       , _rx_channels(rx_channels)
-      {}
+      {
+        _allow_rrc_sn_across_periods = false;
+        cfg.lookupValue("modem.phy.allow_rrc_sn_across_periods", _allow_rrc_sn_across_periods); 
+      }
 
     /**
      *  Default destructor.
@@ -160,6 +163,7 @@ class MbsfnFrameProcessor {
 
     unsigned _rx_channels;
 
+    bool _allow_rrc_sn_across_periods = false;
     static std::mutex _sched_stop_mutex;
     static std::map<uint8_t, uint16_t> _sched_stops;
 
